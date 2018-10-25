7006a38bbd4c4ffddce2795c2b96fb7d3dd7a333
Brian <brianhkang1@gmail.com>
2018-10-24 20:25:00 -0400

    cleaned up code, added comments to clarify

--- story_backend/db/seeds.rb
+++ story_backend/db/seeds.rb
@@ -4,10 +4,10 @@ Dir.foreach("../story_frontend/doodles") do |doodle|
 end
 
 Story.create(title: "Goldilocks", doodle_id: 3)
-Story.create(title: "Peter Pan", doodle_id: 33)
+Story.create(title: "Peter Pan", doodle_id: 32)
 
 Post.create(story_id: 1, content: "There once was a girl", next_post_ids: "[2]", doodle_id: 3)
 Post.create(story_id: 1, content: "She went to the woods", prev_post_id: 1, doodle_id: 27)
 Post.create(story_id: 1, content: "She hates bears", prev_post_id: 54)
 
-Post.create(story_id: 2, content: "There was once a flying boy", doodle_id: 33)
+Post.create(story_id: 2, content: "There was once a flying boy", doodle_id: 32)
